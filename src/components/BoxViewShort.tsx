21ef5e1ec2673215b58bce83cfcd391cad22d93b
Johnny <johnny.gomes@ufjf.br>
2023-05-29 10:43:06 -0300

    feat: start of add item functionality

--- src/components/BoxViewShort.tsx
+++ src/components/BoxViewShort.tsx
@@ -1,17 +1,57 @@
+import { useEffect, useRef, useState } from 'react';
 import { Box } from '../models/box.model';
 import ItemViewShort from './ItemViewShort';
+import NewItemInput from './NewItemInput';
 
 type Props = { box: Box };
 
 export default function BoxViewShort({ box }: Props) {
+  const [toggleNew, setToggleNew] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const addItemHandler = () => {
+    setToggleNew(true);
+  };
+
+  const cancelAddingHandler = () => {
+    setToggleNew(false);
+  };
+
+  const addedItemHandler = (name: string) => {
+    setToggleNew(false);
+  };
+
+  useEffect(() => {
+    if (toggleNew) {
+      inputRef.current!.focus();
+    }
+  }, [toggleNew]);
 
   return (
     <div
       className={`py-2 px-4 text-gray-700 font-medium bg-${box.color}-400 border-${box.color}-700 hover:bg-${box.color}-300 transition-color duration-300 border-2 rounded min-h-[100px] max-w-[280px] flex flex-col`}
     >
-      <div className='mb-2'>{box.label}</div>
+      <div className="flex flex-row justify-between items-center gap-2 mb-2">
+        <div>{box.label}</div>
+        {!toggleNew && (
+          <button
+            className="text-xl rounded-full h-[32px] w-[32px]"
+            onClick={addItemHandler}
+          >
+            +
+          </button>
+        )}
+      </div>
 
       <div className="flex flex-wrap gap-2">
+        {toggleNew && (
+          <NewItemInput
+            onAdded={addedItemHandler}
+            onCancel={cancelAddingHandler}
+            ref={inputRef}
+          />
+        )}
+
         {box.items.map((item, index) => (
           <ItemViewShort key={index} item={item} />
         ))}
---
+++ src/components/NewItemInput.tsx
@@ -0,0 +1,43 @@
+import React, { useRef, KeyboardEvent, useImperativeHandle } from "react";
+
+type Props = { onAdded: (name: string) => void, onCancel?: () => void }
+
+const NewItemInput = React.forwardRef((props: Props, ref) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useImperativeHandle(ref, () => {
+    return {
+      focus() {
+        inputRef.current!.focus();
+      },
+    };
+  });
+
+  const keyDownHandler = (event: KeyboardEvent): void => {
+    if (event.key == 'Enter') {
+      props.onAdded(inputRef.current!.value);
+      inputRef.current!.value = '';
+    }
+  }
+
+  const blurHandler = () => {
+    if (props.onCancel) {
+      props.onCancel();
+    }
+  };
+
+  return (
+    <div className="flex items-center bg-orange-100 rounded-lg py-1 px-1 hover:bg-orange-50 focus-within:bg-orange-50 transition-colors duration-300">
+      <input
+        className="appearance-none bg-transparent border-none w-full text-gray-700 text-sm font-medium font-sans px-1 leading-tight focus:outline-none"
+        type="text"
+        aria-label="New item"
+        ref={inputRef}
+        onKeyDown={keyDownHandler}
+        onBlur={blurHandler}
+      />
+    </div>
+  )
+});
+
+export default NewItemInput;
