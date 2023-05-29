2838bb277c51cbb27ad8d8d46242e08e509d0289
Johnny <johnny.gomes@ufjf.br>
2023-05-29 09:11:14 -0300

    feat: Adds styled add box button

--- src/components/AddBoxButton.tsx
+++ src/components/AddBoxButton.tsx
@@ -1,7 +1,14 @@
+import boxPlus from '../assets/box-plus.svg';
+
 type Props = {}
 
 export default function AddBoxButton({}: Props) {
   return (
-    <div>AddBoxButton</div>
+    <div>
+      <button type="button" className="bg-secondary hover:bg-orange-300 border-b-2 border-b-secondary text-customdark rounded font-bold py-2 w-[200px] flex items-center justify-center gap-2 transition-colors duration-300">
+        <img src={boxPlus} className='w-[40px]' />
+        <span className='text-lg'>Add Box</span>
+      </button>
+    </div>
   )
 }
