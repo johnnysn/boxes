fb7aac95ad36a82402b23466ad740453b84fe40e
Johnny <johnny.gomes@ufjf.br>
2023-06-30 10:17:33 -0300

    ci: Configure github actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/boxes/'
+  }
+
+  return config
 })
