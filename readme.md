1c8b520623cbb3170e87bd81be7211f861420647
Johnny <johnny.gomes@ufjf.br>
2023-06-30 10:49:19 -0300

    feat: Adds information about the app

--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 # Boxes
 
 A pretty simple React app for you to register the content of your boxes!
+
+## Try it out!
+
+This project is running on GitHub Pages: https://johnnysn.github.io/boxes/
--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -3,10 +3,14 @@ export default function About() {
     <>
       <h2 className="text-lg font-semibold mb-2">About "Boxes"</h2>
 
-      <p>
+      <p className="mb-4">
         I don't have much to say about this app. You just go adding boxes and
         throwing items inside of them. Enjoy yourself and your boxes, my friend.
         I hope you know what a box is. That would help a lot.
+      </p>
+
+      <p>
+        This simple app is being developed by Johnny Gomes.
       </p>
     </>
   );
